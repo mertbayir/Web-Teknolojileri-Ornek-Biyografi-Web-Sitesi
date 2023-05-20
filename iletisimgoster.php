233dedfd12299df2f16b9a55157048da050fecbc
mertbayirs4u <mert_bayir.43@hotmail.com>
2023-05-20 23:16:29 +0300

    Düzenlemeler yapıldı.

--- iletisimgoster.php
+++ iletisimgoster.php
@@ -10,14 +10,14 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #0061fd;" > 
         <div class="container">
-          <a class="navbar-brand"  href="anasayfa.html">Mert BAYIR </a>
+          <a class="navbar-brand"  href="giris.html">Mert BAYIR </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto me-2">
               <li class="nav-item">
-                <a class="nav-link" href="index.html">Hakkımda</a>
+                <a class="nav-link" href="hakkımda.html">Hakkımda</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="ozgecmis.html">Özgeçmişim</a>
--- login.php
+++ login.php
@@ -5,8 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #2af059;" > 
+    <div class="container" >
+      <a class="navbar-brand" href="giris.html">Mert BAYIR </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto me-2">
+          <li class="nav-item">
+            <a class="nav-link" href="hakkımda.html">Hakkımda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="ozgecmis.html">Özgeçmişim</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="sehrim.html">Şehrim</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="ilgialanlari.html">İlgi Alanlarım</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.html"> Login </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="iletisim.html"> İletişim </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
     <?php
 
@@ -47,6 +79,6 @@ else
 ?>
   
 
-  
+  <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
